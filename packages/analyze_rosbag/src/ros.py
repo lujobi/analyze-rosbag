660ea7a06988158416b410df8e6d0d513c9a815c
Luzian Bieri <luzibier@ethz.ch>
2020-05-11 15:44:09 +0200

    completely working using regex

--- packages/analyze_rosbag/src/ros.py
+++ packages/analyze_rosbag/src/ros.py
@@ -2,32 +2,46 @@
 
 import yaml
 import rosbag
-import pyaml
+import json
+import re
 
 import os
 import rospy
 from duckietown import DTROS
-from std_msgs.msg import String
+from rospy_message_converter import  message_converter
 
 class Ros_Analyze(DTROS):
 
     def __init__(self, node_name):
-        # initialize the DTROS parent class
         super(Ros_Analyze, self).__init__(node_name=node_name)
-        # construct publisher
-        self.pub = rospy.Publisher('chatter', String, queue_size=10)
 
     def run(self):
-        # publish message every 1 second
-        with rosbag.Bag('/data/bag.bag', 'r') as bag:
-            info_dict = yaml.load(bag._get_yaml_info())
-            print pyaml.dump(info_dict)
+        lat = {'time': [], 'meas': []}
+
+        find_msg_re = r'^(\[LineDetectorNode\] \d+:\sLatencies:\s)'
+        find_line_re = r'\s+--pub_lines--\s+\|\s+total\s+latency\s+\d+.\d+ ms\s+'
+
+        with rosbag.Bag('/data/'+os.environ['BAGNAME'], 'r') as bag:
+
+            for _, msg, _ in bag.read_messages(topics=['/rosout']):
+                temp = message_converter.convert_ros_message_to_dictionary(msg)
+                                
+                msg_string = temp.get('msg')
+                if temp.get('name') == '/{}/line_detector_node'.format(os.environ['DUCKIEBOT']) and re.search(find_msg_re, temp.get('msg')):                  
+                    time = temp.get('header').get('stamp').get('secs') + temp.get('header').get('stamp').get('nsecs')/1000000000.0
+
+                    for line in msg_string.split('\n'):
+                        if re.search(find_line_re,  line):
+                            snippet = re.findall(find_line_re,  line)[0]
+                            lat['time'].append(time)
+                            lat['meas'].append(re.findall(r'\d+.\d+',snippet)[0])
+
+        with open('/data/{}_latencies.json'.format(os.environ['BAGNAME']), 'w') as file:
+            print(lat)
+            file.write(json.dumps(lat))
+
 
 if __name__ == '__main__':
-    # create the node
     node = Ros_Analyze(node_name='ros_Analyze')
-    # run node
     node.run()
-    # keep spinning
-    rospy.spin()
 
